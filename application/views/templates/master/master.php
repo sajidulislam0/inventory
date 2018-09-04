009cd365631e8b16aa7157b5fb132d3061cba4de
sajidulislam0 <msislam72580@gmail.com>
2018-09-05 00:07:00 +0600

    Design filter search

--- application/views/templates/master/master.php
+++ application/views/templates/master/master.php
@@ -6,18 +6,13 @@
         <meta name="description" content="">
         <meta name="author" content="">
         <link rel="icon" href="../../../../favicon.ico">
-
         <title><?= $title ?></title>
-
         <!-- Bootstrap core CSS -->
         <link rel="stylesheet" href="<?= base_url() ?>public/css/bootstrap.min.css" type="text/css">
-        <!--        <link rel="stylesheet" href="css/all.min.css" type="text/css">-->
+        <!-- <link rel="stylesheet" href="css/all.min.css" type="text/css">-->
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" >
-
-
+        <link rel="stylesheet" href="<?= base_url() ?>public/css/jquery-ui.min.css">
         <link rel="stylesheet" href="<?= base_url() ?>public/css/style.css" type="text/css">
-
-
     </head>
 
 
@@ -71,75 +66,66 @@
                                     <li><a href="#">Link 4</a></li>
                                 </ul>
                             </div>
-
                         </div>
-
                     </aside>
-
-                 
                 </div>
                 <div class="col-md-10 main_content_area">
-				
                     <nav class="navbar navbar-expand-md fixed-top menu-custom">
-					
-
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-		<div class="site-buttons d-inline-block back-btn">
-    <button onclick="goBack()" class="btn green font-size"><i class="fa fa-arrow-alt-circle-left"></i> Go Back</button>
-</div>
-
-            <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-	
-                <ul class="navbar-nav m-auto nav">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="<?= base_url() ?>supplier">Supplier <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="<?= base_url() ?>products">Products</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Latest Knowledge</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">BCS</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Model Test</a>
-                    </li>
-
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Bank</a>
-                        <div class="dropdown-menu" aria-labelledby="dropdown01">
-                            <a class="dropdown-item" href="#">Analytical Abilitiesu</a>
-                            <a class="dropdown-item" href="#">Aptitude Test</a>
-                            <a class="dropdown-item" href="#">Bangladesh Affairs</a>
-                            <a class="dropdown-item" href="#">Daily Science</a>
-                            <a class="dropdown-item" href="#">Economics</a>
-                            <a class="dropdown-item" href="#">Banking and Finance</a>
-                            <a class="dropdown-item" href="#">Latest Knowledge</a>
-                            <a class="dropdown-item" href="#">Mathematics</a>
-                            <a class="dropdown-item" href="#">English</a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="site-buttons d-inline-block back-btn">
+                            <button onclick="goBack()" class="btn green font-size"><i class="fa fa-arrow-alt-circle-left"></i> Go Back</button>
                         </div>
-                    </li>
-                </ul>
-                <form class="form-inline my-2 my-lg-0 search-form-style">
-                    <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
 
+                        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
 
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                </form>
-            </div>
-        </nav>
+                            <ul class="navbar-nav m-auto nav">
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="<?= base_url() ?>supplier">Supplier <span class="sr-only">(current)</span></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?= base_url() ?>products">Products</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Latest Knowledge</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">BCS</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Model Test</a>
+                                </li>
 
-                        <div class="body-wrapper">
-
-                                <?= $main_content ?>
-
-                          
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Bank</a>
+                                    <div class="dropdown-menu" aria-labelledby="dropdown01">
+                                        <a class="dropdown-item" href="#">Analytical Abilitiesu</a>
+                                        <a class="dropdown-item" href="#">Aptitude Test</a>
+                                        <a class="dropdown-item" href="#">Bangladesh Affairs</a>
+                                        <a class="dropdown-item" href="#">Daily Science</a>
+                                        <a class="dropdown-item" href="#">Economics</a>
+                                        <a class="dropdown-item" href="#">Banking and Finance</a>
+                                        <a class="dropdown-item" href="#">Latest Knowledge</a>
+                                        <a class="dropdown-item" href="#">Mathematics</a>
+                                        <a class="dropdown-item" href="#">English</a>
+                                    </div>
+                                </li>
+                            </ul>
+                            <form class="form-inline my-2 my-lg-0 search-form-style">
+                                <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+                                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                            </form>
                         </div>
-                   
+                    </nav>
+
+                    <div class="body-wrapper">
+
+                        <?= $main_content ?>
+
+
+                    </div>
+
                 </div>
             </div>
 
@@ -153,36 +139,36 @@
 
         <script src="<?= base_url() ?>public/js/jquery.validate.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js"></script>
-
+        <script src="<?= base_url() ?>public/js/jquery-ui.min.js"></script>
         <script src="<?= base_url() ?>public/js/validation.js"></script>
         <script src="<?= base_url() ?>public/js/scripts.js"></script>
         <script>
-            function goBack() {
-                window.history.back();
-            }
-            /*Search parametter*/
-            function mySearch() {
-                var input, filter, table, tr, td, i, tdCOunt;
-                input = document.getElementById("myInput");
-                filter = input.value.toUpperCase();
-                table = document.getElementById("myTable");
-                tr = table.getElementsByTagName("tr");
-                for (i = 0; i < tr.length; i++) {
-                    td = tr[i].getElementsByTagName("td")[1];
-                    td1 = tr[i].getElementsByTagName("td")[2];
-                    td2 = tr[i].getElementsByTagName("td")[3];
-                    if (td || td1 || td2) {
-                        if (td.innerHTML.toUpperCase().indexOf(filter) > -1 || td1.innerHTML.toUpperCase().indexOf(filter) > -1 || td2.innerHTML.toUpperCase().indexOf(filter) > -1) {
-                            tr[i].style.display = '';
-                        } else {
-                            tr[i].style.display = 'none';
-                            document.getElementById("not_found_error").innerHTML = "Sorry!! Nothing Found...";
+                                function goBack() {
+                                    window.history.back();
+                                }
+                                /*Search parametter*/
+                                function mySearch() {
+                                    var input, filter, table, tr, td, i, tdCOunt;
+                                    input = document.getElementById("myInput");
+                                    filter = input.value.toUpperCase();
+                                    table = document.getElementById("myTable");
+                                    tr = table.getElementsByTagName("tr");
+                                    for (i = 0; i < tr.length; i++) {
+                                        td = tr[i].getElementsByTagName("td")[1];
+                                        td1 = tr[i].getElementsByTagName("td")[2];
+                                        td2 = tr[i].getElementsByTagName("td")[3];
+                                        if (td || td1 || td2) {
+                                            if (td.innerHTML.toUpperCase().indexOf(filter) > -1 || td1.innerHTML.toUpperCase().indexOf(filter) > -1 || td2.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                                                tr[i].style.display = '';
+                                            } else {
+                                                tr[i].style.display = 'none';
+                                                document.getElementById("not_found_error").innerHTML = "Sorry!! Nothing Found...";
 
-                        }
-                    }
+                                            }
+                                        }
 
-                }
-            }
+                                    }
+                                }
 
         </script>
         <script type="text/javascript">
@@ -197,28 +183,43 @@
         </script>    
 
 
-		<script type="text/javascript">
-		 $(function () {
-		  var windowHeight = window.innerHeight;
-    document.body.style.height = windowHeight + "px";
+        <script type="text/javascript">
+            $(function () {
+                var windowHeight = window.innerHeight;
+                document.body.style.height = windowHeight + "px";
 
-window.addEventListener("resize",setWindowHeight,false);
-alert(windowHeight);
-		});
-		        </script>
+                window.addEventListener("resize", setWindowHeight, false);
+                alert(windowHeight);
+            });
+        </script>
 
-		
-				<script type="text/javascript">
-				$(function () {
-					
-					
-					
-					
-					});
-					
-					
-				</script>
 
-		
+        <script>
+            $(function () {
+                $("#from_date").datepicker();
+                $("#to_date").datepicker();
+            });
+
+
+        </script>
+        <script type="text/javascript">
+            $(document).ready(function () {
+              $('#customRadioInline1').on('change', function () {
+                    var t = document.getElementById("customRadioInline1").value;
+                    var advance_field = document.getElementById('advance_search_wrapper');
+                   
+                if (t == 'advance_search'){
+                 
+                     advance_field.style.display = 'block'
+                } else{
+                    advance_field.style.display = 'none'
+                    
+
+                }
+            });
+            });
+        </script>
+
+
     </body>
 </html>
--- application/views/templates/products/products.php
+++ application/views/templates/products/products.php
@@ -22,30 +22,30 @@
                                 <option value="0">Others</option>
                             </select>
                         </div>     
-
-						<div class="form-group col-md-3 product_decrease_width_1">
+                        <div class="form-group col-md-3 product_decrease_width_1">
                             <label for="exampleInputname">Supplier  Name</label>
                             <select name="status" id="status" class="custom-select">
                                 <option value="0">Sajidul Islam</option>
                                 <option value="0">Parves Huda</option>
                             </select>
-                        </div>
-						
+                        </div>		
                         <div class="form-group col-md-2">
                             <label for="exampleInputname">Total Item</label>
                             <select name="status" id="status" class="custom-select">
                                 <option selected="" disabled="">Product Quantity...</option>
-                                <?php $i = 1;
+                                <?php
+                                $i = 1;
                                 while ($i <= 25) {
                                     echo ' <option value="' . $i . '">' . $i . '</option>';
                                     $i++;
-                                } ?>
+                                }
+                                ?>
                             </select>
                         </div>    
 
-						<div class="form-group col-md-2">
+                        <div class="form-group col-md-2">
                             <label for="exampleInputname">Product Unit</label>
-                              <select name="status" id="status" class="custom-select">
+                            <select name="status" id="status" class="custom-select">
                                 <option value="0">Sajidul Islam</option>
                                 <option value="0">Parves Huda</option>
                             </select>
@@ -67,12 +67,45 @@
             </div>
         </form>
     </div>
-
 </div>
 <div class="row mt-5">
     <div class="col-md-12">
         <div class="float-right form-group input-form">
             <input type="text" id="myInput" class="form-control" onkeyup="mySearch()" placeholder="Search for names.." title="Type in a name">
+        </div>
+        <div class="global_search_filter input-form">
+
+            <form method="POST" action="">
+
+                <div class="form-row">
+                    <div class="form-group align-self-end col-md-3">
+                        <div class="custom-control custom-radio custom-control-inline">
+                            <input type="radio" id="customRadioInline1" name="customRadioInline1" class="custom-control-input" value="advance_search">
+                            <label class="custom-control-label" for="customRadioInline1">Advance Search</label>
+                        </div>
+                        <div class="custom-control custom-radio custom-control-inline">
+                            <input type="radio" id="customRadioInline2" name="customRadioInline1" class="custom-control-input" value="normal_search">
+                            <label class="custom-control-label" for="customRadioInline2">Normal search</label>
+                        </div>
+                    </div>
+ </div>
+
+                    <div id="advance_search_wrapper">
+                         <div class="form-row">
+                        <div class="form-group col-md-2" >
+                            <label for="from_date">From date</label>
+                            <input type="text" id="from_date" class="form-control">                      
+                        </div>
+                        <div class="form-group col-md-2">
+                            <label for="to_date">To date</label>
+                            <input type="text" id="to_date" class="form-control">
+                        </div>
+                    </div>
+               
+                    </div>
+               
+            </form>
+
         </div>
         <div class="table-responsive">
             <table class="table table-bordered table-striped" id="myTable">
@@ -82,8 +115,7 @@
                         <th>Product Name:</th>
                         <th>Company Name</th>
                         <th>Supplier Name</th>
-                        <th style=" width: 25%; text-align: center;">Price</th>
-   
+                        <th style="width: 25%; text-align: center;">Price</th>
                         <th>Stock Availabity</th>
                         <th>Action</th>
                     </tr>
@@ -95,23 +127,23 @@
                         <td>unilivercompany@gmail.com</td>
                         <td>0174354856</td>
                         <td>
-						<table class="table mb-0">
-						<tr>
-								<td style="text-align: center;">Buy price - 15 Tk / KG</td>
-								<td style="text-align: center;">Sell price - 17 Tk / Pics</td>
-						</tr>
-						</table>
-						</td>
-						 <td>15 /kG In stock</td>
+                            <table class="table mb-0">
+                                <tr>
+                                    <td style="text-align: center;">Buy price - 15 Tk / KG</td>
+                                    <td style="text-align: center;">Sell price - 17 Tk / Pics</td>
+                                </tr>
+                            </table>
+                        </td>
+                        <td>15 /kG In stock</td>
                         <td align="center">
-						<a href="#" class="btn btn-primary minimum-btn btn-xs"> <i class="fas fa-edit"></i></a>
-						<a href="#" class="btn btn-danger minimum-btn btn-xs"> <i class="fas fa-trash"></i></a>
-						</td>
+                            <a href="#" class="btn btn-primary minimum-btn btn-xs"> <i class="fas fa-edit"></i></a>
+                            <a href="#" class="btn btn-danger minimum-btn btn-xs"> <i class="fas fa-trash"></i></a>
+                        </td>
                     </tr>
                 </tbody>
             </table>
 
-			</div>
+        </div>
         <div>
         </div>
     </div>
